7df151e95469d64c23567fc10a310eedba089fd1
MaykonWSF <maykonwillyam038@gmail.com>
2024-03-12 20:38:25 -0300

    Teste das buscas cegas

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
-import java.util.Map;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Problema problema = new Problema("Arad", "Bucharest");
-        Busca busca = new Busca(problema);
+        Problema problema1 = new Problema("Arad", "Bucharest");
+        Problema problema2 = new Problema("Arad", "Bucharest");
+        
+        Busca busca1 = new Busca(problema1);
+        Busca busca2 = new Busca(problema2);
 
 
-        System.out.println(busca.buscaEmLargura().solucao());
+        System.out.println(busca1.buscaEmLargura().solucao());
+        System.out.println(busca2.buscaEmProfundidade().solucao());
     }
 }
